91c62c932122582571925c360eee894862e08ac0
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-08-14 21:55:54 +0200

    Border Top
    Added white border top to the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import { FaSquareGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="p-4 md:p-8 lg:p-10 bg-gray-800">
+    <footer className="p-4 md:p-8 lg:p-10 bg-gray-800 border-t-white border-t-[2px]">
       <div className="flex justify-start items-center max-w-screen-xl text-center">
         <FaSquareGithub className="text-gray-200 text-xl mr-2" />
         <p className="my-6 text-gray-200 hover:underline">
