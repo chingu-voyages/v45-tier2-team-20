348e74d84117fcc932a19f7733ebda3bd0e04fda
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-08-14 21:59:13 +0200

    Background Image, fontFamily
    Updated Tailwind confing with new fonts: spaceGrotesk,,robotoSlab and with starting backgroundImage

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
       colors: {
         primary: "#5955DF",
       },
+      backgroundImage: {
+        "starting-banner": 'url("../public/banner-image.jpg")',
+      },
+      fontFamily: {
+        spaceGrotesk: ["Space Grotesk", "sans-serif"],
+        robotoSlab: ["Roboto Slab", "sans-serif"],
+      },
     },
   },
   plugins: [],
