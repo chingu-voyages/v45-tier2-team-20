3cf25a2b1681b8cac2b8556cff05181aefd6a5d0
chrioniep <eponde26@gmail.com>
2023-08-15 23:55:33 +0100

    fix padding on footer, create card ui, and update search result page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,9 +1,15 @@
 import { SearchComponent } from "../components/User/SearchComponent/SearchComponent";
+import { Card } from "../components/UI/Card";
 
 export const SearchPage = () => {
-    return (
-        <div className="w-full h-[100vh] grid content-center">
-            <SearchComponent />
-        </div>
-    )
-}
+  return (
+    <div className="w-full h-[100vh] content-center">
+      <div className="bg-indigo-700 h-56 relative">
+        {/* Card  */}
+        <Card className="absolute shadow-lg shadow-indigo-300 h-[150px] w-[50%] top-[9em] left-[22em] mx-auto">
+          <SearchComponent />
+        </Card>
+      </div>
+    </div>
+  );
+};
