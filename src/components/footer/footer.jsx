938c0f15b5a4892e70c279d0e11e774fae519389
chrioniep <eponde26@gmail.com>
2023-08-15 23:13:20 +0100

    change the padding of the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,11 +2,15 @@ import { FaSquareGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="p-4 md:p-8 lg:p-10 bg-gray-800 border-t-white border-t-[2px]">
+    <footer className="p-2 bg-gray-800 border-t-white border-t-[2px]">
       <div className="flex justify-start items-center max-w-screen-xl text-center">
         <FaSquareGithub className="text-gray-200 text-xl mr-2" />
         <p className="my-6 text-gray-200 hover:underline">
-          <a href="https://github.com/chingu-voyages/v45-tier2-team-20">
+          <a
+            href="https://github.com/chingu-voyages/v45-tier2-team-20"
+            target="_blank"
+            rel="noreferrer"
+          >
             Team project github repository
           </a>
         </p>
