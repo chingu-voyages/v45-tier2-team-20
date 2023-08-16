89ca5bdf2f95c4ccbb21c75576d739a27acbd976
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-08-16 19:43:59 +0200

    test

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,3 +1,7 @@
-export const Button = ({text, ...attrs}) => {
-  return <button {...attrs}>{text}</button>
-}
+export const Button = ({ text, ...attrs }) => {
+  return (
+    <div>
+      <button {...attrs}>{text}</button>
+    </div>
+  );
+};
