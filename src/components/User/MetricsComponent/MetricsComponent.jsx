4618d30fe0a686278ec2c9ea7d006474c2dd9870
Irina <sidoren-irina@yandex.ru>
2023-08-22 22:57:21 +0300

    Add Metrics component

---
+++ src/components/User/MetricsComponent/MetricsComponent.jsx
@@ -0,0 +1,14 @@
+import {Card} from "../../UI/Card.jsx";
+
+export const MetricsComponent = () => {
+  return (
+    <div className="flex justify-between gap-4 my-[120px] px-[20px]">
+      <Card className="shadow-lg shadow-indigo-300 h-[150px] w-[50%] mx-auto">
+        <div></div>
+      </Card>
+      <Card className="shadow-lg shadow-indigo-300 h-[150px] w-[50%] mx-auto">
+        <div></div>
+      </Card>
+    </div>
+  )
+}
