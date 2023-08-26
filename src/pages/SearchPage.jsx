4daa116dc81f935b9eb595568b63efcbd6dc8c42
chrioniep <eponde26@gmail.com>
2023-08-26 20:11:37 +0100

    finish implementing detail list component

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,17 +1,21 @@
 import { SearchComponent } from "../components/User/SearchComponent/SearchComponent";
 import { Card } from "../components/UI/Card";
-import {MetricsComponent} from "../components/User/MetricsComponent/MetricsComponent.jsx";
+import { MetricsComponent } from "../components/User/MetricsComponent/MetricsComponent.jsx";
+import { DataListComponent } from "../components/User/DataSummaryComponent/DataListComponent";
 
 export const SearchPage = () => {
   return (
-    <div className="w-full min-h-screen content-center">
+    <div className="w-full min-h-screen content-center bg-[#F7FAFF]">
       <div className="bg-indigo-700 h-56 relative">
         {/* Card  */}
         <Card className="absolute shadow-lg shadow-indigo-300 h-[150px] w-[50%] top-[9em] left-[22em] mx-auto">
           <SearchComponent />
         </Card>
       </div>
-      <MetricsComponent/>
+
+      <DataListComponent />
+
+      {/* <MetricsComponent /> */}
     </div>
   );
 };
