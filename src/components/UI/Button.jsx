57ee8396d8957b7fa17de1cecd3ef4594cc3a26c
Nisha <nishavijai@gmail.com>
2023-08-27 14:06:46 +0200

    Update SearchComponent style

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,3 +1,12 @@
-export const Button = ({ text, ...attrs }) => {
-  return <button {...attrs}>{text}</button>;
+import PropTypes from "prop-types";
+
+export const Button = ({ text, children, ...attrs }) => {
+  return (
+    <button {...attrs}>{text ? text : children}</button>
+  );
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  children: PropTypes.element
 };
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,17 +1,14 @@
 import { SearchComponent } from "../components/User/SearchComponent/SearchComponent";
 import { Card } from "../components/UI/Card";
-import {MetricsComponent} from "../components/User/MetricsComponent/MetricsComponent.jsx";
 
 export const SearchPage = () => {
   return (
-    <div className="w-full min-h-screen content-center">
-      <div className="bg-indigo-700 h-56 relative">
-        {/* Card  */}
-        <Card className="absolute shadow-lg shadow-indigo-300 h-[150px] w-[50%] top-[9em] left-[22em] mx-auto">
+    <div className="h-[100vh] w-full">
+      <div className="relative bg-indigo-700 h-56">
+        <Card className="absolute shadow-md shadow-indigo-500 top-[10vh] left-[8vw] h-[20vh] w-[80vw] md:w-[40vw] md:left-[30vw]">
           <SearchComponent />
         </Card>
       </div>
-      <MetricsComponent/>
     </div>
   );
 };
