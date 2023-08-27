4a24c93cfe437486d69d1dc310f8b717a0d71684
chrioniep <eponde26@gmail.com>
2023-08-27 20:50:03 +0100

    fix design issue

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,14 +1,20 @@
 import { SearchComponent } from "../components/User/SearchComponent/SearchComponent";
 import { Card } from "../components/UI/Card";
+import { DataListComponent } from "../components/User/DataSummaryComponent/DataListComponent";
 
 export const SearchPage = () => {
   return (
-    <div className="h-[100vh] w-full">
-      <div className="relative bg-indigo-700 h-56">
-        <Card className="absolute shadow-md shadow-indigo-500 top-[10vh] left-[8vw] h-[20vh] w-[80vw] md:w-[40vw] md:left-[30vw]">
+    <div className="w-full min-h-screen content-center bg-[#F7FAFF]">
+      <div className="bg-indigo-700 h-56 relative">
+        {/* Card  */}
+        <Card className="absolute shadow-lg shadow-indigo-300 h-[150px] w-[50%] top-[9em] left-[22em] mx-auto">
           <SearchComponent />
         </Card>
       </div>
+
+      <DataListComponent />
+
+      {/* <MetricsComponent /> */}
     </div>
   );
 };
