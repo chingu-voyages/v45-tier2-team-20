6657e46dcfe592c5d231681bc86b2832a2244570
Irina <sidoren-irina@yandex.ru>
2023-09-05 22:29:17 +0300

    Add Metrics component

--- src/components/User/MetricsComponent/MetricsComponent.jsx
+++ src/components/User/MetricsComponent/MetricsComponent.jsx
@@ -1,3 +1,109 @@
-export const MetricsComponent = () => {
+import {Card} from "../../UI/Card.jsx";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import {useEffect, useState} from "react";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const yearOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+      display: false,
+    },
+    title: {
+      display: true,
+      text: "Number of strikes by year",
+    },
+  },
+};
+
+const classOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+      display: false,
+    },
+    title: {
+      display: true,
+      text: "Number of strikes by recClass",
+    },
+  },
+};
+
+
+export const MetricsComponent = () => {
+  const [strikeByYear, setStrikeByYear] = useState([{}]);
+  const [strikeByClass, setStrikeByClass] = useState([{}]);
+  const [metheroite, setMetheroite] = useState([]);
+
+  useEffect(() => {
+    fetch("https://data.nasa.gov/resource/gh4g-9sfh.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setMetheroite(data); // all metheroites
+      });
+  }, []);
+
+  useEffect(() => {
+    const countByYear = {};
+    metheroite.map((item) => {
+      countByYear[new Date(item.year).getFullYear()] =
+        (countByYear[new Date(item.year).getFullYear()] || 0) + 1;
+    });
+    setStrikeByYear(countByYear); // prepare object to the strike by year graph
+    const countByClass = {};
+    metheroite.map((item) => {
+      countByClass[item.recclass] = (countByClass[item.recclass] || 0) + 1;
+    });
+    setStrikeByClass(countByClass); // prepare object to the strike by recclass graph
+  }, [metheroite])
+
+  const yearData = {
+    datasets: [
+      {
+        label: "Number of strikes by year",
+
+        data: strikeByYear,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  const classData = {
+    datasets: [
+      {
+        label: "Number of strikes by recClass",
+
+        data: strikeByClass,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <div className="flex justify-between my-[120px] px-[20px]">
+      <Card className="shadow-md shadow-indigo-200 w-full text-black p-2">
+        <Bar options={yearOptions} data={yearData} className="mb-4" />
+        <Bar options={classOptions} data={classData} />
+      </Card>
+    </div>
+  )
 }
