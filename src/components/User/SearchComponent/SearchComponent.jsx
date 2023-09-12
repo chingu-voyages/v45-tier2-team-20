c81ff9a10e07042af87dad96c64586b95a3828e2
Nisha <nishavijai@gmail.com>
2023-09-12 18:49:20 +0200

    Update search component with placeholder

--- src/components/User/SearchComponent/SearchComponent.jsx
+++ src/components/User/SearchComponent/SearchComponent.jsx
@@ -12,16 +12,43 @@ export const SearchComponent = () => {
     const [meteoriteComposition, setMeteoriteComposition] = useState([]);
     const [massRange, setMassRange] = useState([]);
 
+    const [placeholderValue, setPlaceholderValue] = useState("Search meteor data...");
+
     const { meteoriteData, searchInputValue, setSearchInputValue } = useApi();
 
-    // const filteredListOfMeteoriteNameData = meteoriteData.filter((meteorite) => {
-    //     return meteorite.name.toLowerCase().includes(searchInputValue.toLowerCase());
-    // })
+    let filteredListOfMeteoriteNameData;
+    let filteredListOfMeteoriteYearOfStrikeData;
+    let filteredListOfMeteoriteCompositionData;
+    let filteredListOfMeteoriteMassRangeData;
+
+    if (searchInputValue !== undefined) {
+        filteredListOfMeteoriteNameData = meteoriteData.filter((meteorite) => {
+            return meteorite.name.toLowerCase().includes(searchInputValue.toLowerCase());
+        })
+        filteredListOfMeteoriteYearOfStrikeData = meteoriteData.filter((meteorite) => {
+            return new Date(meteorite.year).getFullYear().toString().includes(searchInputValue);
+        })
+        filteredListOfMeteoriteCompositionData = meteoriteData.filter((meteorite) => {
+            return meteorite.recclass.toLowerCase().includes(searchInputValue.toLowerCase());
+        })
+        filteredListOfMeteoriteMassRangeData = meteoriteData.filter((meteorite) => {
+            return ((meteorite.mass) / 1000).toString().includes(searchInputValue);
+        })
+    }
 
     const handleOnSubmit = (e) => {
         e.preventDefault();
         setSearchInputValue(searchInputValue);
         console.log("searchInputValue: ", searchInputValue);
+        setName(filteredListOfMeteoriteNameData);
+        console.log("name: ", name);
+        setYearOfStrike(filteredListOfMeteoriteYearOfStrikeData);
+        console.log("yearOfStrike: ", yearOfStrike);
+        setMeteoriteComposition(filteredListOfMeteoriteCompositionData);
+        console.log("meteoriteComposition: ", meteoriteComposition);
+        setMassRange(filteredListOfMeteoriteMassRangeData);
+        console.log("massRange: ", massRange);
+        setSearchInputValue("");
     }
 
     const handleOnChange = (e) => {
@@ -30,41 +57,66 @@ export const SearchComponent = () => {
     }
 
     const clearSearchInputValue = (e) => {
-        setSearchInputValue(e.target.value = "");
+        setSearchInputValue("");
+        setName("");
+        setYearOfStrike("");
+        setMeteoriteComposition("");
+        setMassRange("");
         console.log("Clear input value: ", e.target.value);
     }
 
-    useEffect(() => {
-        const getName = [];
-        const getYear = [];
-        const getMeteoriteComposition = [];
-        const getMassRange = [];
-        meteoriteData.map((meteor) => {
-            getName.push(meteor.name);
-            getYear.push(new Date(meteor.year).getFullYear());
-            getMeteoriteComposition.push(meteor.recclass);
-            getMassRange.push((meteor.mass) / 1000);
-        })
-        setName(getName);
-        setYearOfStrike(getYear);
-        setMeteoriteComposition(getMeteoriteComposition);
-        setMassRange(getMassRange);
-    }, [meteoriteData]);
-
     const handleOnClickName = () => {
+        setPlaceholderValue(placeholderTextSwitch("name"));
+        searchInputValue && setSearchInputValue(searchInputValueSwitch(name));
         console.log("getMeteoritesNameData: ", name);
     }
 
     const handleOnClickYearOfStrike = () => {
+        setPlaceholderValue(placeholderTextSwitch("year"));
+        searchInputValue && setSearchInputValue(searchInputValueSwitch(yearOfStrike));
         console.log("getMeteoritesYearOfStrikeData: ", yearOfStrike);
     }
 
     const handleOnClickMeteoriteComposition = () => {
+        setPlaceholderValue(placeholderTextSwitch("composition"));
+        searchInputValue && setSearchInputValue(searchInputValueSwitch(meteoriteComposition));
         console.log("getMeteoritesCompositionData: ", meteoriteComposition);
     }
 
     const handleOnClickMassRangeInKilograms = () => {
+        setPlaceholderValue(placeholderTextSwitch("mass"));
+        searchInputValue && setSearchInputValue(searchInputValueSwitch(massRange));
         console.log("getMeteoritesMassRangeData: ", massRange);
+    }
+
+    const placeholderTextSwitch = (text) => {
+        switch (text) {
+            case "name":
+                return "Search by meteor name (Ex: Aarhus)";
+            case "year":
+                return "Search by meteor year of strike (Ex: 1880)";
+            case "composition":
+                return "Search by meteorite composition (Ex: 'Iron' or 'H4')";
+            case "mass":
+                return "Search by meteorite mass range (Ex: 1.44)";
+            default:
+                return "Search...";
+        }
+    }
+
+    const searchInputValueSwitch = (searchValue) => {
+        switch (searchValue) {
+            case name:
+                return name;
+            case yearOfStrike:
+                return yearOfStrike;
+            case meteoriteComposition:
+                return meteoriteComposition;
+            case massRange:
+                return massRange;
+            default:
+                return name;
+        }
     }
 
     return (
@@ -73,8 +125,7 @@ export const SearchComponent = () => {
                 <section className="relative flex-1 justify-center">
                     <Label htmlFor="search" className="sr-only" text="Search"></Label>
                     <BsSearch style={{ color: "rgb(99 102 241)", height: "20px", width: "20px", position: "absolute", top: "34%", left: "2%" }} />
-                    <Input type="text" id="search" className="p-4 pl-12 pr-14 text-md text-slate-800 rounded-lg bg-white focus:ring-2 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500  placeholder-gray-400 shadow-md shadow-indigo-200 w-full md:text-lg md:pl-14 md:pr-20" value={searchInputValue} onChange={handleOnChange} required />
-
+                    <Input type="text" id="search" className="p-4 pl-12 pr-14 text-md text-slate-800 rounded-lg bg-white focus:ring-2 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500  placeholder-gray-400 shadow-md shadow-indigo-200 w-full md:text-lg md:pl-14 md:pr-20" placeholder={placeholderValue} name={placeholderValue} value={searchInputValue} onChange={handleOnChange} required />
                     <Button className="absolute top-[1vh] right-[1vw]" onClick={clearSearchInputValue}>
                         <IoCloseCircle style={{ color: "rgb(99 102 241)", height: "30px", width: "30px" }} />
                     </Button>
@@ -89,16 +140,6 @@ export const SearchComponent = () => {
                 <Button text="Meteorite Composition" type="button" className="border-solid border-2 border-indigo-400 rounded-md hover:text-slate-800 hover:border-indigo-900 focus:border-transparent focus:ring-2 focus:outline-none focus:ring-indigo-900 focus:text-slate-800 px-6 py-1 mt-3 md:mt-0" onClick={handleOnClickMeteoriteComposition} />
                 <Button text="Mass range" type="button" className="border-solid border-2 border-indigo-400 rounded-md hover:text-slate-800 hover:border-indigo-900 focus:border-transparent focus:ring-2 focus:outline-none focus:ring-indigo-900 focus:text-slate-800 px-6 py-1 mt-3 md:mt-0" onClick={handleOnClickMassRangeInKilograms} />
             </section>
-
-            {/* <section className="mt-12">
-                {filteredListOfMeteoriteNameData.map((meteor) => {
-                    return (
-                        <>
-                            <p key={meteor.id} className="text-indigo-700">{meteor.name} {new Date(meteor.year).getFullYear()} {(meteor.mass) / 1000} {meteor.recclass}</p>
-                        </>
-                    )
-                })}
-            </section> */}
         </section>
     )
 }
