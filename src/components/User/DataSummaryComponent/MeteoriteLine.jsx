5d901415f1dde2a074a8c5b68bcde2dbddaba587
Irina <sidoren-irina@yandex.ru>
2023-09-13 20:58:38 +0300

    Delete metrics component detail

--- src/components/User/DataSummaryComponent/MeteoriteLine.jsx
+++ src/components/User/DataSummaryComponent/MeteoriteLine.jsx
@@ -10,7 +10,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import { Link } from "react-router-dom";
 
 export const MeteoriteLine = ({ data }) => {
   const [country, setCountry] = useState("");
@@ -30,91 +29,89 @@ export const MeteoriteLine = ({ data }) => {
     findCountry();
   }, [data]);
   return (
-    <Link to={`/meteorite/${data.id}`}>
-      <div className="w-full border-[1px] border-gray-300 flex p-5 rounded-md cursor-pointer hover:shadow-lg duration-100">
-        <div className="w-full">
-          <div className="flex items-center space-x-6 mb-4 justify-between">
-            {/* <div className={`w-24 h-24 bg-[#E7E7F0] rounded-md`}></div> */}
-            <h2 className="text-black text-3xl font-semibold">{data.name}</h2>
-            <div className="w-[58px] bg-indigo-600 rounded-full h-[35px] flex items-center justify-center text-white text-semibold text-lg">
-              {data.id}
+    <div className="w-full border-[1px] border-gray-300 flex p-5 rounded-md cursor-pointer hover:shadow-lg duration-100">
+      <div className="w-full">
+        <div className="flex items-center space-x-6 mb-4 justify-between">
+          {/* <div className={`w-24 h-24 bg-[#E7E7F0] rounded-md`}></div> */}
+          <h2 className="text-black text-3xl font-semibold">{data.name}</h2>
+          <div className="w-[58px] bg-indigo-600 rounded-full h-[35px] flex items-center justify-center text-white text-semibold text-lg">
+            {data.id}
+          </div>
+        </div>
+        <div className="grid lg:grid-cols-4 md:grid-cols-cols-2 grid-cols-4 gap-x-6 gap-y-12">
+          <div className="">
+            <div className="flex items-center space-x-3">
+              <FaWeightHanging className="text-gray-400" />
+              <span className="text-gray-400">Mass</span>
+            </div>
+            <h3 className="font-semibold text-black text-md">
+              {data.mass}kg
+            </h3>
+          </div>
+          <div className="">
+            <div className="flex items-center space-x-3">
+              <RxDashboard className="text-gray-400" />
+              <span className="text-gray-400">Recclass</span>
+            </div>
+            <h3 className="font-semibold text-black text-md">
+              {data.recclass}
+            </h3>
+          </div>
+          <div className="">
+            <div className="flex items-center space-x-3">
+              <BsCalendar3 className="text-gray-400" />
+              <span className="text-gray-400">Year</span>
+            </div>
+            <h3 className="font-semibold text-black text-md">
+              {new Date(data.year).getFullYear()}
+            </h3>
+          </div>
+          <div className="">
+            <div className="flex items-center space-x-3">
+              <PiMapPinFill className="text-gray-400" />
+              <span className="text-gray-400">Location</span>
+            </div>
+            {!loading && (
+              <h3 className="font-semibold text-black text-md">{country}</h3>
+            )}
+            {loading && <Skeleton width={90} height={20} />}
+          </div>
+          <div className="">
+            <div className="flex items-center space-x-3">
+              <MdEditDocument className="text-gray-400" />
+              <span className="text-gray-400">Nametype</span>
+            </div>
+            <div className="w-[75px] bg-[#2cbf2948] rounded-full h-[22px] flex items-center justify-center text-green-700 text-semibold text-md">
+              {data.nametype}
             </div>
           </div>
-          <div className="grid lg:grid-cols-4 md:grid-cols-cols-2 grid-cols-4 gap-x-6 gap-y-12">
-            <div className="">
-              <div className="flex items-center space-x-3">
-                <FaWeightHanging className="text-gray-400" />
-                <span className="text-gray-400">Mass</span>
-              </div>
-              <h3 className="font-semibold text-black text-md">
-                {data.mass}kg
-              </h3>
+          <div className="">
+            <div className="flex items-center space-x-3">
+              <FaWind className="text-gray-400" />
+              <span className="text-gray-400">Fall</span>
             </div>
-            <div className="">
-              <div className="flex items-center space-x-3">
-                <RxDashboard className="text-gray-400" />
-                <span className="text-gray-400">Recclass</span>
-              </div>
-              <h3 className="font-semibold text-black text-md">
-                {data.recclass}
-              </h3>
+            <h3 className="font-semibold text-black text-md">{data.fall}</h3>
+          </div>
+          <div className="">
+            <div className="flex items-center space-x-3">
+              <FaArrowsAltV className="text-gray-400" />
+              <span className="text-gray-400">Reclat</span>
             </div>
-            <div className="">
-              <div className="flex items-center space-x-3">
-                <BsCalendar3 className="text-gray-400" />
-                <span className="text-gray-400">Year</span>
-              </div>
-              <h3 className="font-semibold text-black text-md">
-                {new Date(data.year).getFullYear()}
-              </h3>
+            <h3 className="font-semibold text-black text-md">
+              {data.reclat}
+            </h3>
+          </div>
+          <div className="">
+            <div className="flex items-center space-x-3">
+              <FaArrowsAltH className="text-gray-400" />
+              <span className="text-gray-400">Reclong</span>
             </div>
-            <div className="">
-              <div className="flex items-center space-x-3">
-                <PiMapPinFill className="text-gray-400" />
-                <span className="text-gray-400">Location</span>
-              </div>
-              {!loading && (
-                <h3 className="font-semibold text-black text-md">{country}</h3>
-              )}
-              {loading && <Skeleton width={90} height={20} />}
-            </div>
-            <div className="">
-              <div className="flex items-center space-x-3">
-                <MdEditDocument className="text-gray-400" />
-                <span className="text-gray-400">Nametype</span>
-              </div>
-              <div className="w-[75px] bg-[#2cbf2948] rounded-full h-[22px] flex items-center justify-center text-green-700 text-semibold text-md">
-                {data.nametype}
-              </div>
-            </div>
-            <div className="">
-              <div className="flex items-center space-x-3">
-                <FaWind className="text-gray-400" />
-                <span className="text-gray-400">Fall</span>
-              </div>
-              <h3 className="font-semibold text-black text-md">{data.fall}</h3>
-            </div>
-            <div className="">
-              <div className="flex items-center space-x-3">
-                <FaArrowsAltV className="text-gray-400" />
-                <span className="text-gray-400">Reclat</span>
-              </div>
-              <h3 className="font-semibold text-black text-md">
-                {data.reclat}
-              </h3>
-            </div>
-            <div className="">
-              <div className="flex items-center space-x-3">
-                <FaArrowsAltH className="text-gray-400" />
-                <span className="text-gray-400">Reclong</span>
-              </div>
-              <h3 className="font-semibold text-black text-md">
-                {data.reclong}
-              </h3>
-            </div>
+            <h3 className="font-semibold text-black text-md">
+              {data.reclong}
+            </h3>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,6 @@ import LandingPage from "../pages/LandingPage";
 import { SearchPage } from "../pages/SearchPage";
 import MetheroiteDetailPage from "../pages/metheroite/[metheroiteId]/MetheroiteDetailPage";
 import LayoutPage from "../pages/LayoutPage";
-import {MeteoriteDetailPage} from "../pages/meteorite/[meteoriteId]/MeteoriteDetailPage.jsx";
 // import { Navbar } from "../components/UI/Navbar";
 
 export default function Routers() {
@@ -19,10 +18,6 @@ export default function Routers() {
           <Route
             path="/metheroite/:metheroiteId"
             element={<MetheroiteDetailPage />}
-          />
-          <Route
-            path="/meteorite/:meteoriteId"
-            element={<MeteoriteDetailPage />}
           />
         </Route>
       </Routes>
