3046946aa256f006c5ac8ec14f4e571d95e2fa38
chrioniep <eponde26@gmail.com>
2023-09-14 10:03:02 +0100

    set loading in the global variable and implementing sorting

--- src/components/UI/ToggleButton.jsx
+++ src/components/UI/ToggleButton.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { HiChevronUp, HiChevronDown } from "react-icons/hi2";
 
-export const ToggleButton = ({ name }) => {
+export const ToggleButton = ({ name, onClick }) => {
   const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+    if (onClick) {
+      onClick();
+    }
+  };
   return (
     <button
-      onClick={() => setToggle(!toggle)}
+      onClick={handleClick}
       type="button"
       className="bg-white  text-indigo-600 border border-indigo-600 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 flex items-center"
     >
